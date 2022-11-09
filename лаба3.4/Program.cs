5d05210507d0d6be15912e83dfbd2825077958c5
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-09 19:49:56 +0200

    Добавьте файлы проекта.

---
+++ лаба3.4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Горлачев базовый уровень 
+double x = 2;
+double y = 0;
+do 
+{
+    y = Math.Abs(Math.Log(x)) - Math.Pow(x - 2, 2);
+}
+while ((x += 0.2) <= 4.1) ;
+Console.WriteLine("Ответ: "+y);
